3b778e7203e08f00c42ad0bc26af6d30111bc79d
Prashilchaudhari <prashil.chaudhari@gmail.com>
2023-08-26 20:33:36 +0530

    added

--- script_demo.js
+++ script_demo.js
@@ -1,1 +1,14 @@
 console.log("Hello...!!")
+console.log("It is recommited")
+
+
+
+
+
+// echo "# Git_Demo" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Prashilchaudhari/Git_Demo.git
+// git push -u origin main
